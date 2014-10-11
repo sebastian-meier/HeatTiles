df39eee82dee97823c08fa45bf5376207542e8dc
sebastian-meier <contact@sebastianmeier.eu>
2014-10-11 12:27:59 +0200

    SQL-Update, Multiple Datasets, Timseries-Data
    Switch from the old php-sql-interface mysqli.
    A lot of performance upgrades through better SQL-queries and other
    improvements.
    The converter can now deal with multiple datasets in one database, as
    well as time series data.
    The conversion table is now a standalone table, the original source
    data table is kept untouched, this improves the performance especially
    on big datasets.

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php
 
+$session_name = 'heattile_conversion';
+session_name($session_name);
+session_start();
+
 //This feature is really important
 //If the data later on added to your database
 //seams to be not precise enough, it might be
@@ -19,6 +23,7 @@ $db_user = "YOUR_USER_NAME";
 $db_pass = "YOUR_PASSWORD";
 $db_database = "YOUR_DATABASE_NAME";
 $db_table = "YOUR_DATABASE_TABLE_NAME";
+$db_prefix = "";
 
 //For the conversion we need a latitude & longitude column
 //If you have only one column which holds both numbers,
@@ -29,10 +34,15 @@ $db_lng = "longitude";
 //Also you table needs to have a column with a unique id
 $db_id = "id";
 
+//We need an additional table to store the conversions
+$db_conversion_table = "conversions_table";
+
 //We need an additional table to store the maximum amount
 //of locations per square per zoom-level
 $db_max_table = "locations_max_table";
 
+$db_max_table = $db_prefix.$db_max_table;
+$db_conversion_table = $db_prefix.$db_conversion_table;
 
 //If the conversion process taking longer then your server
 //timeout, modify the following value, making it less rows
@@ -46,6 +56,11 @@ set_time_limit(30000);
 $redirect = false;
 $message = false;
 
+/*------------------- ZOOM LEVEL DATA -------------------*/
+
+$zoom_min = 1;
+$zoom_max = 21;
+
 //The data is rasterized into a tiled grid
 //The tile-size in zoom-level "0" is set here:
 $gridsize = 2.5;
@@ -56,32 +71,125 @@ $gridsize = 2.5;
 $max = 20037508;
 $step_size = array();
 $steps = array();
-for($i = 0; $i<20; $i++){
+for($i = $zoom_min; $i<$zoom_max; $i++){
 	array_push($step_size, ($gridsize * pow(2, $i)));
 	array_push($steps, round($max*2.0/$step_size[$i]));
 }
 
+/*------------------- TIMESERIES DATA -------------------*/
+
+$time = true;
+$time_col = "NAME OF THE COLUMN WITH THE TIME REFERENCE";
+
+/*------------------- MULTIPLE DATASETS -------------------*/
+
+//If your dataset holds multiple subdatasets, that you want to visualize seperately 
+//You need to have another database table at hand that holds unique identifiers to filter the main location database
+
+$multiple = true;
+$multiple_db = "NAME OF DATABASE";
+$multiple_id_col = "NAME OF COLUMN WITH UNIQUE IDENTIFIERS WITHIN MULTIPLE_DB";
+$multiple_ref_col = "NAME OF COLUMN WITH UNIQUE IDENTIFIERS WITHIN DB_TABLE";
+
+/*------------------- GROUPING / CLUSTERING -------------------*/
+
+//You can use a set of clustering methods
+//The names of the methods are equivalent to the SQL methods used in the queries (maybe this helps to understand what is happening here)
+
+//"COUNT" will add "COUNT_MAX", "COUNT_MIN", "COUNT_AVG"
+//"COUNT_MAX"
+//"COUNT_MIN"
+//"COUNT_AVG"
+//Count all Locations per Grid-Cell
+
+/*---------*/
+//If you want to use one of the following methods you need to provide the "cluster_value_col"
+$cluster_value_col = "NAME OF THE COLUMN WITHIN DB_TABLE that holds the VALUE";
+
+//"SUM"
+//Sum up all values
+//Example: Crimes commited per location, returns the number of crimes per Grid-Cell
+
+//"MAX"
+//The highest value of one location per Grid-Cell
+//Example: Earthquakes - each location has the current magnitude, returns the highest magnitude per Grid-Cell
+
+//"MIN"
+//The lowest value of one location per Grid-Cell
+//Example: Groundwater level - each location has the current groundwater level, returns the lowest level per Grid-Cell
+
+//"AVG"
+//The average value of one location per Grid-Cell
+//Example: Average Income - each location has the average income, returns the average income per Grid-Cell
+
+$clustering = array("COUNT", "MAX", "SUM", "MIN", "AVG");
+
+if(in_array("COUNT", $clustering)){
+    array_push($clustering, "COUNT_MAX");
+    array_push($clustering, "COUNT_MIN");
+    array_push($clustering, "COUNT_AVG");
+}
+
 /*------------------- MYSQL -------------------*/
 
-if(!$link = mysql_connect($db_server, $db_user, $db_pass)){
-    echo 'Problem connecting to the MySql Server';
-    exit;
+$mysqli = mysqli_connect($db_server, $db_user, $db_pass, $db_database);
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
 
-if(!mysql_select_db($db_database, $link)){
-    echo 'Unable to connect to database-table ';
-    exit;
+function sql_request($request, $requestArray){
+    global $mysqli;
+    
+    if(!$requestArray){
+        
+        $result = $mysqli->query($request);
+
+    }else{
+
+        if(!is_array($requestArray)){
+            $requestArray = array($requestArray);
+        }
+
+        $stmt = $mysqli->prepare($request);
+
+        $types = "";
+        foreach ($requestArray as $value) {
+            if(is_integer($value)){
+                $types .= "i";
+            }elseif (is_double($value)) {
+                $types .= "d";
+            }else{
+                $types .= "s";
+            }
+        }
+        $params = array($types);
+
+        foreach ($requestArray as $value) {
+            array_push($params, $value);
+        }
+
+        $r = call_user_func_array(array($stmt, "bind_param"), refValues($params));
+
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+    }
+
+    return($result);
 }
 
-function query_mysql($sql, $link){
-	$result = mysql_query($sql, $link);
-	if (!$result) {
-    	echo "DB Error, could not execute request\n";
-    	echo 'MySQL Error: ' . mysql_error();
-    	exit;
-	}else{
-		return $result;
-	}
+function refValues($arr){
+        $refs = array();
+
+        foreach ($arr as $key => $value)
+        {
+            $refs[$key] = &$arr[$key]; 
+        }
+
+        return $refs; 
 }
 
 /*------------------- CONVERSION FUNCTIONS -------------------*/
--- convert.php
+++ convert.php
@@ -1,13 +1,24 @@
-<html>
-<head>
-</head><?php
+<?php
 
-require_once("config.php");
+require_once("config_local.php");
 
-$sql = 'SELECT * FROM `'.$db_table.'`';
-$result = mysql_query($sql, $link);
-$db_size = mysql_num_rows($result);
-mysql_free_result($result);
+if(!isset($_SESSION["last_id"])){
+	$_SESSION["last_id"] = 0;
+}
+
+if(!isset($_SESSION["db_size"])){
+	$sql = 'SELECT COUNT(*) FROM `'.$db_table.'`';
+	$result = sql_request($sql, false);
+	$row = $result->fetch_row();
+	$db_size = $row[0];
+	$_SESSION["db_size"] = $db_size;
+	$result->close();
+	$mysqli->next_result();
+	$message .= $db_size.' (new)<br />';
+}else{
+	$db_size = $_SESSION["db_size"];
+	$message .= $db_size.' (stored)<br />';
+}
 
 if(isset($_GET["step"])){$step = $_GET["step"];}else{$step = 0;}
 if(isset($_GET["page"])){$page = $_GET["page"];}else{$page = 1;}
@@ -16,8 +27,9 @@ if($step==0){
 	$message .= 'Welcome to the data conversion and rasterization.<br />';
 	$message .= '<a href="convert.php?step=1">Start Process</a>';
 }else if($step==1){
+
 /*------------------- STEP #1 -------------------*/
-//In the first step we need to add new fields to the database
+//In the first step we need to add a table to the database
 //COLUMNS:
 
 //Lat/Lng-Conversion in WebMercator Projection
@@ -36,136 +48,419 @@ if($step==0){
 //Zoom-Level 0-20, grid with offset in rows for hexagon-visualization
 //zh0 - zh20, bigint(20)
 
-	$sql = 'ALTER TABLE `'.$db_table.'` DROP `validconversion`, DROP `x`, DROP `y`, DROP `x0`, DROP `y0`';
-	for($i = 1; $i<21; $i++){ $sql .=  ', DROP `z'.$i.'`'; }
-	for($i = 1; $i<21; $i++){ $sql .=  ', DROP `zh'.$i.'`'; }
-	$result = mysql_query($sql, $link);
+	$sql = 'CREATE TABLE `'.$db_conversion_table.'` (';
+	$columns = array('ref_id', 'x', 'y', 'x0', 'y0');
+	for($i = $zoom_min; $i<$zoom_max; $i++){ array_push($columns, 'z'.$i); }
+	for($i = $zoom_min; $i<$zoom_max; $i++){ array_push($columns, 'zh'.$i); }
 
-	$sql = 'ALTER TABLE `'.$db_table.'` ADD `validconversion` tinyint(1) NOT NULL, ADD `x` DOUBLE(21,12) NOT NULL, ADD `y` DOUBLE(21,12) NOT NULL, ADD `x0` DOUBLE(21,12) NOT NULL, ADD `y0` DOUBLE(21,12) NOT NULL';
-	for($i = 1; $i<21; $i++){ $sql .=  ', ADD `z'.$i.'` BIGINT(20) NOT NULL'; }
-	for($i = 1; $i<21; $i++){ $sql .=  ', ADD `zh'.$i.'` BIGINT(20) NOT NULL'; }
-	$result = query_mysql($sql, $link);
-	if ($result) {
-		$message .= 'Datebase was successfully modified!<br />';
-		$message .= '<a href="convert.php?step=2">Continue</a>';
+	foreach ($columns as $column) {
+		if($column == 'ref_id'){
+			$specs = ' INT(10) NOT NULL';
+		}elseif(($column == 'y')||($column == 'x')||($column == 'y0')||($column == 'x0')){
+			$specs = ' DOUBLE(21,12) NULL';
+		}else{
+			$specs = ' BIGINT(20) NULL';
+		}
+		$sql .= ' `'.$column.'` '.$specs.', ';
 	}
-	mysql_free_result($result);
 
-//Flag Rows that have no valid latitude / longitude values
-	$sql = 'UPDATE `'.$db_table.'` SET `validconversion` = 1';
-	$result = query_mysql($sql, $link);
-	mysql_free_result($result);
+	$sql .= 'UNIQUE ( `ref_id` ))';
 
-	$sql = 'UPDATE `'.$db_table.'` SET `validconversion` = 0 WHERE `'.$db_lat.'` = 0 OR `'.$db_lng.'` = 0 OR `'.$db_lng.'` > 180 OR `'.$db_lat.'` > 90';
-	$result = query_mysql($sql, $link);
-	mysql_free_result($result);
+	$result = sql_request($sql, false);
+	if(!$result){
+		printf("Error: %s\n", $mysqli->error);
+		exit();
+	}
+
+	$result = sql_request('TRUNCATE TABLE `'.$db_conversion_table.'`', false);
+	if($result){
+		$_SESSION["last_id"] = 0;
+		$message .= 'Datebase was successfully created!<br />';
+		$message .= '<a href="convert.php?step=2">Continue</a>';
+	}else{
+		printf("Error: %s\n", $mysqli->error);
+		exit();
+	}
 
 }else if($step==2){
 /*------------------- STEP #2 -------------------*/
 //In the First Conversion Step we convert all Latitude/Longitude values to WebMercator
 
-	$sql = 'SELECT `'.$db_id.'`, `'.$db_lat.'`, `'.$db_lng.'` FROM `'.$db_table.'` ORDER BY `'.$db_id.'` ASC LIMIT '.(($page-1)*$db_max).', '.$db_max;
-	$result = query_mysql($sql, $link);
-	if ($result) {
-		while ($row = mysql_fetch_assoc($result)) {
+	$end = false;
+	$msql = '';
+	$msqls = array();
+	$sql = 'SELECT `'.$db_id.'`, `'.$db_lat.'`, `'.$db_lng.'` FROM `'.$db_table.'` WHERE `ref_id` > '.$_SESSION["last_id"].' ORDER BY `'.$db_id.'` ASC LIMIT '.$db_max;
+	$result = sql_request($sql, false);
+	if ($result && $result->num_rows >= 1) {
+		while ($row = $result->fetch_assoc()) {
+			if($row[$db_lng] < -180){
+				$row[$db_lng] = 360 + $row[$db_lng];
+			}
+	    	
 	    	$latlon = ToWebMercator($row[$db_lat], $row[$db_lng]);
-	    	$sql = 'UPDATE `'.$db_table.'` SET `x` = '.$latlon[1].', `y` = '.$latlon[0].', `x0` = '.($latlon[1]+$max).', `y0` = '.($latlon[0]+$max).' WHERE `'.$db_id.'` = '.$row[$db_id];
-			$update = query_mysql($sql, $link);
-			mysql_free_result($update);
+	    	$msql .= 'INSERT INTO `'.$db_conversion_table.'` (`x`, `y`, `x0`, `y0`, `ref_id`)VALUES('.$latlon[1].', '.$latlon[0].', '.($latlon[1]+$max).', '.($latlon[0]+$max).', '.$row[$db_id].');';
+
+	    	if(strlen($msql)>(1048576*0.75)){
+				array_push($msqls, $msql);
+				$msql = "";
+			}
 		}
-		if(($page*$db_max) > $db_size){
-			$message .= 'Conversion to WebMercator was successfully!<br />';
-			$message .= '<a href="convert.php?step=3">Continue</a>';
-		}else{
-			$redirect = 'convert.php?step=2&page='.($page+1);
-			$message .= $page.'/'.ceil($db_size/$db_max).'<br />';
-		}
+		array_push($msqls, $msql);
+	}elseif($result->num_rows<1){
+		$end = true;
+	}else{
+		printf("Error: %s\n", $mysqli->error);
+		exit();
 	}
-	mysql_free_result($result);
+	$result->close();
+	$mysqli->next_result();
+
+	foreach ($msqls as $msql) {
+		exec_multi($msql);		
+	}
+
+	if($end){
+		$_SESSION["last_id"] = 0;
+		$message .= 'Conversion to WebMercator was successfully!<br />';
+		$message .= '<a href="convert.php?step=3">Continue</a>';
+	}else{
+		$redirect = 'convert.php?step=2&page='.($page+1);
+		$message .= $page.'/'.ceil($db_size/$db_max).'<br />';
+	}
 
 }else if($step==3){
 /*------------------- STEP #3 -------------------*/
 //Rasterizing locations into the tiled grid
 
-	$sql = 'SELECT `'.$db_id.'`, `x0`, `y0` FROM `'.$db_table.'` WHERE `validconversion` = 1 ORDER BY `'.$db_id.'` ASC LIMIT '.(($page-1)*$db_max).', '.$db_max;
-	$result = query_mysql($sql, $link);
-	if ($result) {
-		while ($row = mysql_fetch_assoc($result)) {
-	    	//normal tiles
-			for($i = 0; $i<20; $i++){
-				$x = ceil($row["x0"]/$step_size[19-$i]);
-				$y = ceil($row["y0"]/$step_size[19-$i]);
-				$cell = (($y-1)*$steps[19-$i]+$x); //-1
-				$sql = 'UPDATE `'.$db_table.'` SET `z'.($i+1).'` = '.$cell.' WHERE `'.$db_id.'` = '.$row[$db_id];
-				$update = query_mysql($sql, $link);
-				mysql_free_result($update);
-			}
+	$end = false;
+	$msqls = array();
+	$msql = "";
+	$sql = 'SELECT `ref_id`, `x0`, `y0` FROM `'.$db_conversion_table.'` WHERE `ref_id` > '.$_SESSION["last_id"].' ORDER BY `ref_id` ASC LIMIT '.$db_max;
+	$result = sql_request($sql, false);
+	if ($result && $result->num_rows >= 1) {
+		while ($row = $result->fetch_assoc()) {
+			$tsql = "UPDATE `".$db_conversion_table."` SET ";
+			$ftsql = true;
+			for($i = $zoom_min; $i<$zoom_max; $i++){
 
-			//offset tiles (hex tiles 50% y-offset)
-			for($i = 0; $i<20; $i++){
-				$y = ceil($row["y0"]/$step_size[19-$i]);
+		    	//normal tiles
+				$x = ceil($row["x0"]/$step_size[($zoom_max-1)-$i]);
+				$y = ceil($row["y0"]/$step_size[($zoom_max-1)-$i]);
+				$cell = (($y-1)*$steps[($zoom_max-1)-$i]+$x); //-1
+
+				//offset tiles (hex tiles 50% y-offset)
+				$y = ceil($row["y0"]/$step_size[($zoom_max-1)-$i]);
 				if($y%2){
-					$x = ceil(($row["x0"]+$step_size[19-$i]/2)/$step_size[19-$i]);
+					$x = ceil(($row["x0"]+$step_size[($zoom_max-1)-$i]/2)/$step_size[($zoom_max-1)-$i]);
 				}else{
-					$x = ceil($row["x0"]/$step_size[19-$i]);
+					$x = ceil($row["x0"]/$step_size[($zoom_max-1)-$i]);
 				}
-				$cell = (($y-1)*$steps[19-$i]+$x); //-1
-				$sql = 'UPDATE `'.$db_table.'` SET `zh'.($i+1).'` = '.$cell.' WHERE `'.$db_id.'` = '.$row[$db_id];
-				$update = query_mysql($sql, $link);
-				mysql_free_result($update);
+				$cellh = (($y-1)*$steps[($zoom_max-1)-$i]+$x); //-1
+				
+				if($ftsql){
+					$ftsql = false;
+				}else{
+					$tsql .= ' , ';
+				}
+
+				$tsql .= '`zh'.$i.'` = '.$cellh.', `z'.$i.'` = '.$cell;
+
+			}
+
+			$tsql .= ' WHERE `ref_id` = '.$row["ref_id"].';';
+			$_SESSION["last_id"] = $row["ref_id"];
+
+			$msql .= $tsql;
+
+			if(strlen($msql)>(1048576*0.75)){
+				array_push($msqls, $msql);
+				$msql = "";
 			}
 
 		}
-		if(($page*$db_max) > $db_size){
-			$message .= 'Rasterization successfully!<br />';
-			$message .= '<a href="convert.php?step=4">Continue</a>';
-		}else{
-			$message .= $page.'/'.ceil($db_size/$db_max).'<br />';
-			$redirect = 'convert.php?step=3&page='.($page+1);
-		}
+		array_push($msqls, $msql);
+	}elseif($result->num_rows<1){
+		$end = true;
+	}else{
+		printf("Error: %s\n", $mysqli->error);
+		exit();
 	}
-	mysql_free_result($result);
+	$result->close();
+	$mysqli->next_result();
+
+	foreach ($msqls as $msql) {
+		exec_multi($msql);		
+	}
+
+	if($end){
+		$message .= 'Rasterization successfully!<br />';
+		$message .= '<a href="convert.php?step=4">Continue</a>';
+	}else{
+		$message .= $page.'/'.ceil($db_size/$db_max).'<br />';
+		$redirect = 'convert.php?step=3&page='.($page+1);
+	}
+
 
 }else if($step==4){
 /*------------------- STEP #4 -------------------*/
 //Finally we need to find out the maximum amount of locations per tile per zoom level and store it in a new database.
+//If you added more features than "SUM" to $clustering than this will be done here as well.
 
 //Create new database if it doesn't exists yet
 
-	$sql = 'CREATE TABLE IF NOT EXISTS `'.$db_max_table.'` (`id` int(11) NOT NULL AUTO_INCREMENT, `zoom` int(11) NOT NULL, `key` text COLLATE latin1_german2_ci NOT NULL, `value` text COLLATE latin1_german2_ci NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci';
-	$result = query_mysql($sql, $link);
-	mysql_free_result($result);
-
-//Clear Database
-	$sql = 'TRUNCATE TABLE `'.$db_max_table;
-	$result = query_mysql($sql, $link);
-	mysql_free_result($result);
-
-//Now add zoom level values
-	for($i=1; $i<21; $i++){
-		$sql = 'SELECT COUNT(*) FROM `'.$db_table.'` WHERE `validconversion` = 1 GROUP BY `z'.$i.'` ORDER BY COUNT(*) DESC LIMIT 0,1';
-		$result = query_mysql($sql, $link);
-		if($result) {
-			while ($row = mysql_fetch_array($result)) {
-				$sql = 'INSERT INTO `'.$db_max_table.'` (`zoom`, `value`, `key`)VALUES('.$i.', '.$row[0].', "max")';
-				$insert = query_mysql($sql, $link);
-				mysql_free_result($insert);
-			}
+	if($page < 1){
+		$sql = 'CREATE TABLE IF NOT EXISTS `'.$db_max_table.'` (`id` int(11) NOT NULL AUTO_INCREMENT, `ref_id` int(11) NOT NULL, `time` int(11) NOT NULL, `zoom` int(11) NOT NULL, `key` text NOT NULL, `value` DOUBLE(21,12) NULL, PRIMARY KEY (`id`))';
+		$result = sql_request($sql, false);
+		if(!$result){
+			printf("Error: %s\n", $mysqli->error);
+			exit();
 		}
-		$sql = 'SELECT COUNT(*) FROM `'.$db_table.'` WHERE `validconversion` = 1 GROUP BY `zh'.$i.'` ORDER BY COUNT(*) DESC LIMIT 0,1';
-		$result = query_mysql($sql, $link);
-		if ($result) {
-			while ($row = mysql_fetch_array($result)) {
-				$sql = 'INSERT INTO `'.$db_max_table.'` (`zoom`, `value`, `key`)VALUES('.$i.', '.$row[0].', "maxh")';
-				$insert = query_mysql($sql, $link);
-				mysql_free_result($insert);
-			}
+
+	//Clear Database
+		$sql = 'TRUNCATE TABLE `'.$db_max_table;
+		$result = sql_request($sql, false);
+		if(!$result){
+			printf("Error: %s\n", $mysqli->error);
+			exit();
 		}
-		mysql_free_result($result);
 	}
 
-	$message .= 'Summary done!<br />';
-	$message .= '<a href="convert.php?step=5">Continue</a>';
+	$sql = "SELECT";
+	
+	if($time){
+		$sql .= " `count_max`.`time` AS `time`,";
+	}
+
+	if($multiple){
+		$sql .= " `count_max`.`id` AS `id`,";
+	}
+
+	$sql .= "\n";
+
+	if(in_array("COUNT", $clustering)||in_array("COUNT_MAX", $clustering)||in_array("COUNT_MIN", $clustering)||in_array("COUNT_AVG", $clustering)){
+		$sql .= " 					SUM(`count_max`.`countmax`) AS `count`, 
+					MAX(`count_max`.`countmax`) AS `max`, 
+					MIN(`count_max`.`countmax`) AS `min`, 
+					AVG(`count_max`.`countmax`) AS `avg`,";
+					$sql .= "\n";
+	}
+
+	if(in_array("SUM", $clustering)||in_array("MIN", $clustering)||in_array("MAX", $clustering)||in_array("AVG", $clustering)){
+		$sql .= "					SUM(`count_max`.`sum_value`) AS `value_sum`,
+					MIN(`count_max`.`min_value`) AS `value_min`,
+					MAX(`count_max`.`max_value`) AS `value_max`,
+					AVG(`count_max`.`avg_value`) AS `value_avg`";
+					$sql .= "\n";
+	}
+
+	$sql .= " 	FROM 
+				(SELECT ";
+
+	if($time){
+		$sql .= " `ref_table`.`".$time_col."` AS `time`,";
+	}
+
+	if($multiple){
+		$sql .= " `ref_table`.`".$multiple_ref_col."` AS `id`,";
+	}
+
+	if(in_array("COUNT", $clustering)||in_array("COUNT_MAX", $clustering)||in_array("COUNT_MIN", $clustering)||in_array("COUNT_AVG", $clustering)){
+		$sql .= '	COUNT(*) AS `countmax`,';
+	}
+	if(in_array("SUM", $clustering)||in_array("MIN", $clustering)||in_array("MAX", $clustering)||in_array("AVG", $clustering)){
+		$sql .= '	SUM(`ref_table`.`'.$cluster_value_col.'`) AS `sum_value`,
+					MIN(`ref_table`.`'.$cluster_value_col.'`) AS `min_value`,
+					MAX(`ref_table`.`'.$cluster_value_col.'`) AS `max_value`,
+					AVG(`ref_table`.`'.$cluster_value_col.'`) AS `avg_value`';
+    }
+
+    $sql .= ' 	FROM 
+					`'.$db_conversion_table.'` 
+				INNER JOIN 
+					`'.$db_table.'` AS `ref_table` 
+				ON `ref_table`.`'.$db_id.'` =  `'.$db_conversion_table.'`.`ref_id` 
+				GROUP BY ';
+
+	if($time){
+		$sql .= '`ref_table`.`'.$time_col.'`,';
+	}
+
+	if($multiple){
+		$sql .= '`ref_table`.`'.$multiple_ref_col.'`,';
+	}
+
+    $sql .= '`z'.($page+$zoom_min).'`
+              ) 
+			AS 
+				`count_max`';
+
+	if($time||$multiple){
+		$sql .= " GROUP BY ";
+		if($time){
+			$sql .= '`count_max`.`time`';
+		}
+
+		if($time && $multiple){
+			$sql .= ",";
+		}
+
+		if($multiple){
+			$sql .= '`count_max`.`id`';
+		}
+    }
+
+
+	$msql = '';
+	$msqls = array();
+
+	$result = sql_request($sql, false);
+	if($result){
+	    while($row = $result->fetch_assoc()){
+
+			foreach ($row as $key => $value) {
+				$id = 0;
+				$time = 0;
+				if(isset($row["id"])){
+					$id = $row["id"];
+				}
+				if(isset($row["time"])){
+					$time = $row["time"];
+				}
+				if(($key != "time")&&($key != "id")){
+					$msql .= 'INSERT INTO `'.$db_max_table.'` (`zoom`, `value`, `key`, `ref_id`, `time`)VALUES('.($page+$zoom_min).', '.$value.', "'.$key.'", '.$id.', '.$time.');';
+					if(strlen($msql)>(1048576*0.75)){
+						array_push($msqls, $msql);
+						$msql = "";
+					}
+				}				
+			}
+			
+		}
+		array_push($msqls, $msql);
+	}else{
+	    printf("Error: %s\n", $mysqli->error);
+		exit();
+	}
+
+	$result->close();
+	$mysqli->next_result();
+
+	foreach ($msqls as $msql) {
+		exec_multi($msql);		
+	}
+
+	if($page+$zoom_min < $zoom_max){
+
+		if($time){
+			//Through the interval we calculated the meta data for every individual time frame
+			//Now we need to Calculate the overall meta data 
+
+			$msql = '';
+			$msqls = array();
+			for($i = $zoom_min; $i<$zoom_max; $i++){
+				foreach ($clustering as $value) {
+					switch ($value) {
+						case 'COUNT_MAX':
+							$selection = 'MAX(`value`)';
+							$where = 'max';
+							break;
+
+						case 'COUNT_MIN':
+							$selection = 'MIN(`value`)';
+							$where = 'min';
+							break;
+
+						case 'COUNT_AVG':
+							$selection = 'AVG(`value`)';
+							$where = 'avg';
+							break;
+
+						case 'COUNT':
+							$selection = 'SUM(`value`)';
+							$where = 'count';
+							break;
+
+						case 'MAX':
+							$selection = 'MAX(`value`)';
+							$where = 'value_max';
+							break;
+
+						case 'MIN':
+							$selection = 'MIN(`value`)';
+							$where = 'value_min';
+							break;
+
+						case 'AVG':
+							$selection = 'AVG(`value`)';
+							$where = 'value_avg';
+							break;
+
+						case 'SUM':
+							$selection = 'SUM(`value`)';
+							$where = 'value_sum';
+							break;
+						
+						default:
+							$selection = 'COUNT(`value`)';
+							$where = 'count';
+							break;
+					}
+
+					$msql .= 'SELECT '.$selection.' AS `calc_value`, `key`, `zoom`, `ref_id` FROM `'.$db_max_table.'` WHERE `zoom` = '.$i.' AND `key` = "'.$where.'" GROUP BY `ref_id`;';	
+
+				}
+			}
+
+			if($mysqli->multi_query($msql)){
+				$msql = '';
+				$msqls = array();
+				do {
+					if($result = $mysqli->store_result()){
+						while ($row = $result->fetch_assoc()){
+							$msql .= 'INSERT INTO `'.$db_max_table.'` (`zoom`, `value`, `key`, `ref_id`, `time`)VALUES('.$row["zoom"].', '.$row["calc_value"].', "'.$row["key"].'", '.$row["ref_id"].', -1);';
+
+							if(strlen($msql)>(1048576*0.75)){
+								array_push($msqls, $msql);
+								$msql = "";
+							}
+
+						}
+					}else{
+						printf("Error: %s\n", $mysqli->error);
+						exit();
+					}
+					$result->close();
+
+					if (!$mysqli->more_results()) {
+						break;
+					}
+
+					if (!$mysqli->next_result()) {
+						printf("Error: %s\n", $mysqli->error);
+						exit();
+					}
+
+				} while (true);
+
+			}else{
+				printf("Error: %s\n", $mysqli->error);
+				exit();
+			}
+
+			array_push($msqls, $msql);
+
+			foreach ($msqls as $msql) {
+				exec_multi($msql);		
+			}
+
+		}
+
+		$message .= 'Summary done!<br />';
+		$message .= '<a href="convert.php?step=5">Continue</a>';
+	}else{
+		$message .= ($page+$zoom_min).'/'.$zoom_max.'<br />';
+		$redirect = 'convert.php?step=4&page='.($page+1);
+	}
 
 }else if($step==5){
 /*------------------- STEP #5 -------------------*/
@@ -175,15 +470,23 @@ if($step==0){
 	$message .= '<a href="examples/index.html">Examples</a>';
 }
 
+$mysqli->close();
+
 ?>
+<html>
+<head>
+</head>
 <body onload="redirect()">
 <?php echo $message; ?>
 	<script language=javascript>
 		function redirect(){
-<?php if($redirect){ ?>
+<?php
+if($redirect){
+?>
 			window.location = "<?php echo $redirect; ?>";
-<?php } ?>
+<?php
+}
+?>
 		}
 	</script>
 </body>
-</html>
